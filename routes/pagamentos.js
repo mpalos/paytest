0b6bab0883df7345195f630fddb9c4c6dcdb36ef
Marco Franco <MAPF@linux-q7uq.site>
2017-11-28 21:59:54 -0200

    inclusao do uma rota de post

--- routes/pagamentos.js
+++ routes/pagamentos.js
@@ -4,4 +4,15 @@ module.exports = function(app){
         console.log('Recebida requisição de pagamentos na porta 3000.');
         res.send('OK.');
     });
+
+    app.post('/pagamentos/pagamento', function(req,res){
+        var pagamento = req.body;
+        console.log('Processando requisicao de um novo pagamento');
+
+        pagamento.status = 'CRIADO';
+        pagamento.data = new Date;
+
+
+        res.send(pagamento);
+    });
 };
